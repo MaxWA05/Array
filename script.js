48441b4bc32e7e87498a42eec8af52a57eeb3139
MaxWA05 <max.asheim@gmail.com>
2022-05-06 14:16:40 +0200

    Max

---
+++ script.js
@@ -0,0 +1,24 @@
+//Oppgave 1
+//A) Hensikten med en array er å ha lagret en liste med mange ting under ett variabel navn.
+//B) Javascript arrays kan endres størrelse på lett og kan ha mikset flere datatyper i samme array.
+
+//oppgave 2
+//a)
+const emptyArray = [];
+//b) 
+const Anneby = ["Skrue", "Ole", "Dole", "Doffen", "GuffenGås", "Hetti", "Letti", "Netti", "Donald", "Anton"];
+//c)
+console.log(Anneby[2], Anneby[5], Anneby[8]);
+
+
+//Oppgave 3
+//a) En index i en array er hvor objektet i arrayen er plassert.
+//b) Første index i en array er 0.
+//c) Siste index i array er tallet på hvor lang listen er -1.
+//d) Lengden i array er hvor mange objekter det er i arrayen.
+//e) 
+
+//oppgave 4
+//a) Typeof operatøren returnerer "object" på arrays.
+//b) 
+
